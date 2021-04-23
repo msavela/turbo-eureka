bfe5961aa755df68e9801afd8c4a5a569ac1bba0
Matias Savela <matias.savela@codemate.com>
2021-04-23 08:28:49 +0300

    Added postMessage

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 console.log("turbo-eureka");
 
 document.write("<h1>turbo-eureka</h1>");
+
+window.parent.postMessage(
+  {
+    name: window.name,
+    type: "PluginHeightCheck",
+    height: window.document.body.clientHeight,
+  },
+  "*"
+);
+window.parent.postMessage(
+  { name: window.name, type: "PluginRegisterEvent", slot: slot },
+  "task-details"
+);
